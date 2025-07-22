3bec9fb6574b71fadc9b9fc852d6fe11abb2c3cc
andj712 <andj712@gmail.com>
2025-07-22 19:14:51 +0200

    categories: add mapping and dtos

---
+++ DTOs/ImportCategory/CategoryDto.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace finance_management.DTOs.ImportCategory
+{
+    public class CategoryDto
+    {
+        [Required]
+        [Name("code")]
+        public string Code { get; set; } = string.Empty;
+
+        [Required]
+        [Name("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("parent-code")]
+        [Name("parent-code")]
+        public string? ParentCode { get; set; }
+    }
+}
---
+++ DTOs/ImportCategory/CategoryImportDto.cs
@@ -0,0 +1,8 @@
+﻿namespace finance_management.DTOs.ImportCategory
+{
+    public class CategoryImportDto
+    {
+        public List<CategoryDto> Categories { get; set; } = new();
+    }
+
+}
